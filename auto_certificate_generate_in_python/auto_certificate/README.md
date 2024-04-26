c6574a3d65d2034509c17015f425c3042f198a51
Rohan <121424360+captainrd@users.noreply.github.com>
2024-04-27 00:15:39 +0530

    Add files via upload
    Update code

---
+++ auto_certificate_generate_in_python/auto_certificate/README.md
@@ -0,0 +1,36 @@
+# Cerificate maker
+## About the project
+a python proggram used to automate the making the certificates
+
+
+## Build with 
+- Python3 
+
+## Getting Started
+This is an example of how you may give instructions on setting up your project locally. To get a local copy up and running follow these simple example steps.
+
+## Prerequisites
+
+```cmd
+pip install -r requirements.txt
+```
+## Enviroment used for running 
+- windows 11
+- os had the font installed (not tested without installing the font)
+## Usage
+Command to use
+```cmd
+python3 certificate.py <csv file with names> <font name> <font size on certificate for student name> <certificate template image> <output folder name in current directory>
+
+```
+
+```cmd
+python3 certificate.py list.csv Charm-Bold.ttf 60 Telperium_Updated_Cert.jpg output_certificate
+```
+
+- **list.csv**
+    - has the list of student names in csv format
+
+
+# Contact
+- 
---
+++ auto_certificate_generate_in_python/auto_certificate/certificate.py
@@ -0,0 +1,42 @@
+﻿from PIL import Image,ImageDraw,ImageFont
+import pandas as pd
+import os
+import sys
+
+
+filename = sys.argv[1]
+font_file = sys.argv[2]
+font_size = sys.argv[3]
+font_size = int(font_size)
+input_template_certificate = sys.argv[4]
+output_folder = sys.argv[5]
+
+df = pd.read_csv(filename, encoding = "utf-8")
+
+font = ImageFont.truetype(font_file,font_size)
+
+path = os.getcwd() + "\\" + output_folder
+output_folder_check = os.path.exists(path)
+if(output_folder_check):
+	pass
+else:
+	os.mkdir(path)
+
+img = Image.open(input_template_certificate)
+image_width = img.width
+image_height = img.height
+
+print("**************")
+for index,j in df.iterrows():
+	img = Image.open(input_template_certificate)
+	draw = ImageDraw.Draw(img)
+	message = text='{}'.format(j[0])
+	_, _, w, h = draw.textbbox((0, 0), message, font=font)
+	draw.text(((image_width-w)/2, (image_height-h)/2),text='{}'.format(j[0]),fill=(0,0,0),font=font,align='Left')
+	output_file =  output_folder + '/{}.jpg'.format(j[0])
+	img.save(output_file)
+	print(output_file + " Saved")
+
+print("**************")
+
+# python3 certificate.py list.csv Charm-Bold.ttf 60 certificate_template.jpg output_certificate
